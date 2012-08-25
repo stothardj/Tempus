5ce162786028bf68dcc0bce306e7ec00e0ed37d3
Jake Stothard <stothardj@gmail.com>
2012-08-25 12:14:28 -0700

    Enable popout

--- www/js/initui.js
+++ www/js/initui.js
@@ -1,3 +1,10 @@
 $(function(){
     $('#tabs').tabs();
+    $('#popout').click(function (ev) {
+	var url = $(this).attr('href');
+	var name = 'Tempus';
+	var size = 'width=850,height=650';
+	window.open(url, name, size);
+	ev.preventDefault();
+    });
 });
